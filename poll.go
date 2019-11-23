f06f9dc7fe9a66305d93b84e1894ea80f4c1b47a
EletronSz <janetuker2@gmail.com>
2019-11-23 22:06:12 +0200

    added ignore

--- poll.go
+++ poll.go
@@ -6,7 +6,19 @@ import (
 	"math"
 )
 
+//aa two numbers
+func add(x int, y int) int {
+	return x + y
+}
+
+//substract numbers
+func sub(x int, y int) int {
+return x - y
+}
+
 //main function
 func main() {
 	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(add(50,55))
+	fmt.Println(sub(50,20))
 }
